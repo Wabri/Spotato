31b43541789187e0a140d01acb8d8f56e10fdde6
Wabri <gabriele.puliti@gmail.com>
2019-07-21 21:46:21 +0200

    Add (Spotato.sh): Add Spotato and the installer

---
+++ Spotato.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+case "$1" in
+    play)
+	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
+	;;
+    pause)
+    	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
+    	;;
+    toggle)
+	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+    	;;
+    next)
+	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
+    	;;
+    previous)
+	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
+    	;;
+    *)
+    	echo 'Usage: spotato {play|pause|toggle|next|previous}'
+	exit 2
+
+esac
+
+exit 0
---
+++ install.sh
@@ -0,0 +1,4 @@
+cp Spotato.sh spotato
+chmod +x spotato
+mv spotato /usr/local/bin/spotato
+
