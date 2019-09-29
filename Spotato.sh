92448b3fc7524c7ea506a9a2975d14f8f607e6d8
Wabri <gabriele.puliti@protonmail.com>
2019-09-29 19:19:34 +0200

    Update (installer and spotato script): now installation create a mediaplayer file inside the configuration of user and spotato use it

--- Spotato.sh
+++ Spotato.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+mediaplayer=$(cat ~/.config/spotato/mediaplayer)
+
 case "$1" in
     play)
-	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
+		dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$mediaplayer /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
 	;;
     pause)
-    	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
-    	;;
+   		dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$mediaplayer /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
+   	;;
     toggle)
-	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
-    	;;
+		dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$mediaplayer /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+   	;;
     next)
-	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
-    	;;
+		dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$mediaplayer /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
+   	;;
     previous)
-	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
-    	;;
+		dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$mediaplayer /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
+   	;;
     *)
-    	echo 'Usage: spotato {play|pause|toggle|next|previous}'
-	exit 2
+   		echo 'Usage: spotato {play|pause|toggle|next|previous}'
+		exit 2
 
 esac
 
