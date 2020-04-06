db3ccc43fd98a66e593ed53f5069ae2da4868dda
Wabri <gabriele.puliti@protonmail.com>
2020-04-06 11:18:01 +0200

    Update (install): change default mediaplayer selection
    More controll on mediaplayer choosing

--- install.sh
+++ install.sh
@@ -13,16 +13,19 @@ _create_configuration() {
 
 _setting_up() {
 	mediaplayer=$(head -n 1 ~/.config/spotato/mediaplayer)
-	echo '  ---> What media player do you want to use?'
-	echo '   1) Spotify (default)'
-	echo '   2) Headset'
-	read -p '  ---> Type selection number: ' key
+	if [ -z $mediaplayer ] 
+	then
+		mediaplayer=spotify
+	fi
+	echo '  ---> What media player do you want to use? (you can change even after installation)'
+	echo '   Default mediaplayer: '$mediaplayer
+	read -p '  ---> Press enter or type the mediaplayer you want to controll: ' key
 	case $key in
-		2)
-			mediaplayer=headset
-		;;
-		1|*)
-			mediaplayer=spotify
+		'')
+			;;
+		*)
+			mediaplayer=$key
+			;;
 	esac
 	echo $mediaplayer > ~/.config/spotato/mediaplayer
 }
@@ -38,5 +41,4 @@ echo '---> Extracting the right spotato'
 _setting_up
 echo '******************************************'
 echo '---> Let the music play with spotato'
-
 
